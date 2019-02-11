72b39f2e14824f6edf01a03d7930448bc7cd4926
EGOIST <0x142857@gmail.com>
2019-02-11 20:33:31 +0800

    add icon for external link

--- components/Nav.js
+++ components/Nav.js
@@ -12,7 +12,7 @@ export default withRouter(({ router }) => (
         </Link>
       </li>
       <li>
-        <Link href={{ pathname: '/planning', query: router.query }}prefetch>
+        <Link href={{ pathname: '/planning', query: router.query }} prefetch>
           <a className={router.pathname === '/planning' ? 'active' : null}>
             Planning
           </a>
@@ -22,6 +22,25 @@ export default withRouter(({ router }) => (
         <a href="https://anilist.co/user/egoistlily" target="blank">
           My AniList
         </a>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          aria-hidden="true"
+          x="0px"
+          y="0px"
+          viewBox="0 0 100 100"
+          width="15"
+          height="15"
+          className="external-link-icon"
+        >
+          <path
+            fill="currentColor"
+            d="M18.8,85.1h56l0,0c2.2,0,4-1.8,4-4v-32h-8v28h-48v-48h28v-8h-32l0,0c-2.2,0-4,1.8-4,4v56C14.8,83.3,16.6,85.1,18.8,85.1z"
+          />{' '}
+          <polygon
+            fill="currentColor"
+            points="45.7,48.7 51.3,54.3 77.2,28.5 77.2,37.2 85.2,37.2 85.2,14.9 62.8,14.9 62.8,22.9 71.5,22.9"
+          />
+        </svg>
       </li>
     </ul>
     <select
@@ -33,8 +52,8 @@ export default withRouter(({ router }) => (
         })
       }
     >
-      <option value='anime'>Anime</option>
-      <option value='manga'>Manga</option>
+      <option value="anime">Anime</option>
+      <option value="manga">Manga</option>
     </select>
     <style jsx>{`
       nav {
@@ -64,8 +83,15 @@ export default withRouter(({ router }) => (
       a {
         color: #999;
       }
+      a:hover {
+        color: #666;
+      }
       a.active {
         color: #000;
+      }
+      .external-link-icon {
+        color: #999;
+        margin-left: 3px;
       }
     `}</style>
   </nav>

fab0ea5d0e94215d33973c05b5651e86bb9a8865
EGOIST <0x142857@gmail.com>
2020-03-31 22:19:37 +0800

    add spinner

--- components/MediaList.js
+++ components/MediaList.js
@@ -35,7 +35,41 @@ export default ({ status }) => {
   const { loading, data } = mediaListResult
 
   if (loading || !data) {
-    return <div>Loading...</div>
+    return (
+      <div className="loading">
+        <style jsx>{`
+        .loading {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          margin: 50px 0;
+        }
+        `}</style>
+        <svg
+          width="38"
+          height="38"
+          viewBox="0 0 38 38"
+          xmlns="http://www.w3.org/2000/svg"
+          stroke="#067df7"
+        >
+          <g fill="none" fillRule="evenodd">
+            <g transform="translate(1 1)" strokeWidth="2">
+              <circle strokeOpacity=".5" cx="18" cy="18" r="18" />
+              <path d="M36 18c0-9.94-8.06-18-18-18">
+                <animateTransform
+                  attributeName="transform"
+                  type="rotate"
+                  from="0 18 18"
+                  to="360 18 18"
+                  dur="1s"
+                  repeatCount="indefinite"
+                />
+              </path>
+            </g>
+          </g>
+        </svg>
+      </div>
+    )
   }
 
   const list = getList(
